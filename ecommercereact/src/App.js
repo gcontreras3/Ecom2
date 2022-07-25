8f30a40ab240f71b29a80886f7041c2f4f3bea8e
gcontreras3 <103069981+gcontreras3@users.noreply.github.com>
2022-07-25 16:48:08 -0500

    Added files, server, changed home page, product page, contact page

--- ecommercereact/src/App.js
+++ ecommercereact/src/App.js
@@ -1,18 +1,40 @@
 import Products from './components/Products'
+
 import './App.css';
+import Home from './components/Home'
+import Navbar from './components/Nav'
+import Footer from './components/Footer'
+
+import Contact from './components/Contact'
+import logo from './assets/logo.png'
+import { Routes, Route } from 'react-router-dom'
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        
-        <div className="nav">
-        <a href="About">About</a>
-        <a href="Products">Products</a>
-        <a href="Contact">Contact Page</a>
-      </div>
+    <div className="page-container">
+      
+      <div className="content-wrap">
+      
+      <header className="header-app">
+      
+      <h1 className="title-home">DOUBL <img className="logo" src={logo} alt="logo"/> DRIBBL</h1>
+      <Navbar/>
       </header>
-      <Products />
+      
+        {/* <div className="nav"> */}
+        <React.Fragment>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route exact path="/Products" element={<Products/>}/>
+          <Route exact path="/Contact" element={<Contact/>}/>
+        </Routes>
+        </React.Fragment>
+        
+        {/* </div> */}
+      
+      </div>
+      <Footer/>
     </div>
   );
 }
